befc0f91786ed7627ea6cb937209e7f48cd0e55a
YeJeff <664398564@qq.com>
2017-02-09 15:32:29 +0800

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta(object):
+        model = Post
+        fields = ('title', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +11,47 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_daate = timezone.now()
+            post.save()
+            if request.POST.get('_save'):
+                return redirect('post_details', pk=post.pk)
+            if request.POST.get('_addanother'):
+                return redirect('post_new')
+    else:
+        form = PostForm(instance = post)
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        '''
+        print('*' * 10)
+        print(request.POST)
+        print(request.POST.get('_save', '_save_none'))
+        print(request.POST.get('_addanother', '_addanother_none'))
+        print(request.method)
+        print(dir(request))
+        print(type(request.user))
+        print(request.user)
+        print('*' * 10)
+        '''
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            if request.POST.get('_save'):
+                return redirect('post_details', pk=post.pk)
+            if request.POST.get('_addanother'):
+                form = PostForm()
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
